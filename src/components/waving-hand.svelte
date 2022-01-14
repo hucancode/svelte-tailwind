bd455ff2e4d2afe9822ede2619c6987fcc8f1fa8
hucancode <hucancode@gmail.com>
2022-01-15 00:12:49 +0900

    setup svelte app with tailwindcss

---
+++ src/components/waving-hand.svelte
@@ -0,0 +1,5 @@
+<script>
+	let className;
+    export { className as class };
+</script>
+<span class="animate-waving-hand inline-block origin-[70%_70%] {className}">👋</span>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	import WavingHand from '@components/waving-hand.svelte';
+</script>
+
+<div
+	class="flex flex-col justify-center items-center gap-7
+    w-screen h-screen">
+	<WavingHand class="text-5xl" />
+	<div>
+		<h1 class="text-4xl font-bold text-gray-800">
+			Welcome to <span class="font-logo-cursive">Sveltekit</span>
+		</h1>
+		<p class="text-lg font-thin text-black">
+			Visit <a class="hover:text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+			documentation
+		</p>
+	</div>
+</div>
