afab65957d9b571fd16d145b85169fa420f9e5f2
hucancode <hucancode@gmail.com>
2022-01-15 15:42:02 +0900

    static export option

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
-
+const production = process.env.NODE_ENV === 'production';
 const config = {
 	kit: {
-		adapter: adapter(),
-
+        paths: {
+			base: production ? '/svelte-tailwind' : '',
+		},
+        appDir: 'app',
+		adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
         vite: {
