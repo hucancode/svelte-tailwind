3e7595d602db754117c8554eab8d195a1d5eb207
hucancode <hucancode@gmail.com>
2022-01-15 12:45:22 +0900

    better i18n handling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,15 +2,13 @@
 	import WavingHand from '@components/waving-hand.svelte';
 	import { _ } from 'svelte-i18n';
 </script>
-<div
-	class="flex flex-col justify-center items-center gap-7
-    w-screen h-screen">
+<div class="flex-center gap-7 w-screen h-screen">
 	<WavingHand class="text-5xl" />
-	<div class="text-center">
-		<h1 class="text-4xl font-bold text-gray-800">
-			{$_('welcome')} <span class="font-logo-cursive">Sveltekit</span>
+	<div class="flex-col-center text-center">
+		<h1 class="welcome">
+			{@html $_('welcome')}
 		</h1>
-		<p class="text-lg font-thin text-black">
+		<p class="font-thin text-black">
 			Visit <a class="hover:text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
 			documentation
 		</p>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,25 +6,25 @@ const config = {
 				'3/2': '150%',
 				'2/1': '200%',
 				'4/2': '200%',
-				double: '200%',
+				'double': '200%',
 				'5/2': '250%'
 			},
 			fontFamily: {
 				'logo-cursive': ['"Great Vibes"', 'cursive']
 			},
 			screens: {
-				sm: '640px',
-				md: '768px',
-				lg: '1024px',
-				xl: '1280px',
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
 				'2xl': '1536px'
 			},
 			container: {
 				padding: {
-					DEFAULT: '1rem',
-					sm: '2rem',
-					lg: '4rem',
-					xl: '5rem',
+					'DEFAULT': '1rem',
+					'sm': '2rem',
+					'lg': '4rem',
+					'xl': '5rem',
 					'2xl': '6rem'
 				}
 			},
