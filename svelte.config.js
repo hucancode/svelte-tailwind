d4111e336b6163e98de56cfb7a7dfec0e43624f3
hucancode <hucancode@gmail.com>
2022-01-16 10:54:23 +0900

    small optimize

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,7 @@ import path from 'path';
 const config = {
 	kit: {
 		adapter: adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		target: 'body',
 		vite: {
 			resolve: {
 				alias: {
